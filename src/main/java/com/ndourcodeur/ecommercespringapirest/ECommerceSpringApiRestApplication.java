fc9c3d7d6269a8e59b6b08e3a8f4c287dcfb3fe7
ndourbamba18 <ndourbamba18@gmail.com>
2022-04-29 12:21:13 +0100

    Using Command Line Runner

--- src/main/java/com/ndourcodeur/ecommercespringapirest/ECommerceSpringApiRestApplication.java
+++ src/main/java/com/ndourcodeur/ecommercespringapirest/ECommerceSpringApiRestApplication.java
@@ -1,7 +1,12 @@
 package com.ndourcodeur.ecommercespringapirest;
 
+import com.ndourcodeur.ecommercespringapirest.entities.Product;
+import com.ndourcodeur.ecommercespringapirest.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
@@ -12,10 +17,29 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 public class ECommerceSpringApiRestApplication {
 
+	@Autowired
+	private ProductRepository productRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceSpringApiRestApplication.class, args);
 
 		System.out.println("Server is running....");
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(ProductRepository productRepository){
+		return args -> {
+			productRepository.save(new Product("Hp", "Laptop", 258.45));
+			productRepository.save(new Product("Mac Pro", "Laptop", 400.00));
+			productRepository.save(new Product("Iphone X", "Mobile", 550.00));
+			productRepository.save(new Product("SamSung S21", "Mobile", 350.00));
+			productRepository.save(new Product("SamSung A10", "Mobile", 300.00));
+			productRepository.save(new Product("Acer ", "Laptop", 450.45));
+			productRepository.save(new Product("Lenovo", "Laptop", 258.45));
+			productRepository.save(new Product("Dell", "Laptop", 258.45));
+			productRepository.save(new Product("Hp Medium", "Laptop", 258.45));
+			productRepository.save(new Product("Hp Small", "Laptop", 258.45));
+		};
 	}
 
 }
